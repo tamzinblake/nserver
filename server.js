5efd198e5fc3c2484336cd34bbbcfbea31b94c97
Thom Blake <thom@odonnellpdc.com>
2011-09-12 17:13:49 -0400

    require earlier

--- server.js
+++ server.js
@@ -2,14 +2,18 @@ var express = require('express')
 var conf = require('./etc/conf.js')
 
 var app = express.createServer( express.bodyParser() )
+  , required = {}
 
 for (var i = 0; i < conf.routes.length; i++) {
   var route = conf.routes[i]
   var method = route.method.match(/^(get|post)$/i) ? route.method.toLowerCase()
              : 'get'
+  if (route.name !== undefined) {
+    required[route.name] = require(conf.route_root + route.name)
+  }
   app[method]( route.path
              , route.action !== undefined ? route.action : function (req, res) {
-                 require(conf.route_root + route.name)(req, res, route.method)
+                 required[route.name](req, res, route.method)
                }
              )
 }
